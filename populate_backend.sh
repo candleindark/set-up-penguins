e46268e3cff74f03835b98be729d7d24b7d3ca1b
Isaac To <isaac.chun.to@gmail.com>
2026-02-01 17:42:41 -0800

    feat: download data in penguins dataset

--- populate_backend.sh
+++ populate_backend.sh
@@ -27,6 +27,10 @@ fi
 
 echo "Installing penguins dataset with all subdatasets..."
 micromamba run -n "$POPULATE_ENV_NAME" datalad install -r -s https://hub.datalad.org/edu/penguins.git "$DATASET_DIR"
+
+echo "Downloading all data in the dataset..."
+datalad get -r "$DATASET_DIR"
+
 echo ""
 echo "✅ Dataset installed to: $DATASET_DIR"
 
